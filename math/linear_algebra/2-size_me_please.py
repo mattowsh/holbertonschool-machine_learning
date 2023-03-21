4d948200092096c58b743a77bc3632cbc924c4da
mattowsh <mattobelen@gmail.com>
2023-03-21 17:55:42 -0300

    new idea, through all layers

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -7,13 +7,9 @@ Task 2. Size Me Please
 def matrix_shape(matrix):
     """Calculates the shape of a matrix"""
 
-    qty_layers = len(matrix)
-    qty_rows = len(matrix[0])
-
-    # Check if matrix is 2D or 3D:
-    if isinstance(matrix[0][0], list):
-        qty_cols = len(matrix[0][0])
-        matrix_shape = [qty_layers, qty_rows, qty_cols]
-    else:
-        matrix_shape = [qty_layers, qty_rows]
+    matrix_shape = []
+    while type(matrix) != int:
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    
     return matrix_shape
