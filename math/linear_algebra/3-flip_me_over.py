abf349a543ee60785c0ec45fb676e599734c022c
mattowsh <mattobelen@gmail.com>
2023-03-21 17:08:22 -0300

    task 3. new alternative

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,8 +3,22 @@
 Task 3. Flip Me Over
 """
 
+
 def matrix_transpose(matrix):
-    """Returns the transpose of a 2D matrix"""
-    t_matrix = [[matrix[i][j] for i in range(len(matrix))]
-                for j in range(len(matrix[0]))]
+    """Returns the transpose of a matrix"""
+    # Fst alternative:
+    # t_matrix = [[matrix[i][j] for i in range(len(matrix))]
+    #             for j in range(len(matrix[0]))]
+
+    # Snd alternative:
+    t_matrix = []
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    for i in range(rows):
+        new_row = []
+        for j in range(columns):
+            new_row.append(matrix[j][i])
+            t_matrix.append(new_row)
+
     return (t_matrix)
