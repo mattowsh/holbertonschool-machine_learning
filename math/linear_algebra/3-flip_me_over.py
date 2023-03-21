35c2d1dd6f138c1caf50b1156ff66368db46e9a8
mattowsh <mattobelen@gmail.com>
2023-03-21 16:59:20 -0300

    task 3: documented

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-# Write a function that returns the transpose of a 2D matrix
+"""
+Write a function that returns the transpose of a 2D matrix
+"""
 
 
 def matrix_transpose(matrix):
+
     t_matrix = [[matrix[i][j] for i in range(len(matrix))]
                 for j in range(len(matrix[0]))]
     return (t_matrix)
