eeca4e4f7cfc50a46ab67421425ff20d18af1469
mattowsh <mattobelen@gmail.com>
2023-03-22 14:50:39 -0300

    complete documentation try 1

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -7,7 +7,7 @@ Task 13. Cat's Got Your Tongue
 
 def np_cat(mat1, mat2, axis=0):
     """
-    > Concatenates two matrices along a specific axis
+    > Function that concatenates two matrices along a specific axis
     > mat1 and mat2 can be interpreted as numpy.ndarrays, and never empty
     """
     return np.concatenate((mat1, mat2), axis)
