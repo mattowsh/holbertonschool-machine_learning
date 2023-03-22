fa47b14f6be74e0632e4ebce474f8b90770400a4
mattowsh <mattobelen@gmail.com>
2023-03-22 15:14:29 -0300

    comments were deleted: for conflicrt

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -10,7 +10,4 @@ def np_matmul(mat1, mat2):
     > Funtion that performs matrix multiplication
     > mat1 and mat2 are numpy.ndarrays and never empty
     """
-    # matmul() gives us the matrix product of two 2-d arrays
-    # Is great for times when we’re unsure of what the dimensions of our
-    # matrices will be :)
     return np.matmul(mat1, mat2)
