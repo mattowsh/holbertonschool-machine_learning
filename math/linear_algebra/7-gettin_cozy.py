eedbe78586939ab3f6953e76dca06defa1bcd33e
mattowsh <mattobelen@gmail.com>
2023-03-22 00:42:03 -0300

    returns were fixd

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -13,10 +13,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     # Add the new values as a new row => axis == 0
     if not axis:
         return mat1 + mat2
-    
+
     # Add the new values as a new columns => axis == 1
-    if axis == 1 and len(mat1) == len(mat2):
-        result = []
-        result = [(mat1[i] + mat2[i]) for i in range(len(mat1))]
-    else:
-        return
+    if axis == 1:
+        if len(mat1) == len(mat2):
+            result = []
+            result = [(mat1[i] + mat2[i]) for i in range(len(mat1))]
+            return result
+    return
