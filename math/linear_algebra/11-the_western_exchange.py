7560e9842991e12e135e01ed77f9013519115af8
mattowsh <mattobelen@gmail.com>
2023-03-22 14:48:39 -0300

    task 13 v0

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -6,7 +6,7 @@ Task 11. The Western Exchange
 
 def np_transpose(matrix):
     """
-    > Function that transpose a matrix.
-    > The matrix can be interpreted as a numpy.ndarray.
+    > Function that transpose a matrix
+    > The matrix can be interpreted as a numpy.ndarray
     """
     return matrix.T
---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Task 13. Cat's Got Your Tongue
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    > Concatenates two matrices along a specific axis
+    > mat1 and mat2 can be interpreted as numpy.ndarrays, and never empty
+    """
+    return np.concatenate((mat1, mat2), axis)
