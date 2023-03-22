8fba00d68200375ab1d34ea253fdf0825c9a2a03
mattowsh <mattobelen@gmail.com>
2023-03-22 13:00:55 -0300

    task 11 v0

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Task 11. The Western Exchange
+"""
+
+
+def np_transpose(matrix):
+    """Function that transpose a matrix.
+    The matrix can be interpreted as a numpy.ndarray."""
+    return np.transpose(matrix)
