f65fc264114a3695aee85579d0f5840e66b6a23d
mattowsh <mattobelen@gmail.com>
2023-03-21 23:06:45 -0300

    task 5 v0

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -8,10 +8,10 @@ def add_arrays(arr1, arr2):
     """Adds two arrays element-wise"""
 
     if len(arr1) != len(arr2):
-        return None
+        return
 
     result = []
-    if len(arr1) != 0:
+    if len(arr1):
         for i in range(len(arr1)):
             new_value = arr1[i] + arr2[i]
             result.append(new_value)
---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Task 5. Across The Planes
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise"""
+
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return
+
+    result = []
+    if len(mat1) and len(mat1[0]):
+        for i in range(len(mat1)):
+            new_row = []
+            for j in range(len(mat1[0])):
+                new_row.append(mat1[i][j] + mat2[i][j])
+            result.append(new_row)
+    return result
