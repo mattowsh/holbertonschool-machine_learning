d9924fb2fdb29e2df0692f3a1b3dd14da6f971de
mattowsh <mattobelen@gmail.com>
2023-03-22 13:11:26 -0300

    numpy module was deleted

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 """
 Task 11. The Western Exchange
 """
@@ -8,4 +7,4 @@ Task 11. The Western Exchange
 def np_transpose(matrix):
     """Function that transpose a matrix.
     The matrix can be interpreted as a numpy.ndarray."""
-    return np.transpose(matrix)
+    return matrix.T
