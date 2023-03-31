b188612d0daad4bbc3687b651926e496659d8e1b
mattowsh <mattobelen@gmail.com>
2023-03-31 19:08:59 -0300

    stack overflow, classic equation was added

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -10,11 +10,14 @@ def summation_i_squared(n):
     if type(n) != int or n < 1:
         return
 
-    if n == 1:
-        return n**2
-    else:
-        result = n**2
-        n -= 1
-        result += summation_i_squared(n)
+    return int(n * (n + 1) * (2 * n + 1)/6)
 
-    return result
+    # Recursion work but max calls in Python: 1000, stack overflow :^)
+    # if n == 1:
+    #     return n**2
+    # else:
+    #     result = n**2
+    #     n -= 1
+    #     result += summation_i_squared(n)
+
+    # return result
