aa57afc8456b7221c383861d2f2c4bfe133cde60
mattowsh <mattobelen@gmail.com>
2023-04-14 14:21:48 -0300

    b1 was fixed: zero matrix for any neuron in the hidden layer

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -24,13 +24,13 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         elif nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        else:
-            # Hidden layer > neuron attributes:
-            self.W1 = np.random.normal(size=(nodes, nx))
-            self.b1 = 0
-            self.A1 = 0
-            # Output layer > neuron attributes:
-            # The 1 in size: correspond to the qty of output neurons:
-            self.W2 = np.random.normal(size=(1, nodes))
-            self.b2 = 0
-            self.A2 = 0
+        
+        # Hidden layer > neuron attributes:
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        # Output layer > neuron attributes:
+        # The 1 in size: correspond to the qty of output neurons:
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
