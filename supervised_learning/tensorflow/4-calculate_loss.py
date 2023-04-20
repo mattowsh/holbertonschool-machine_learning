softmax function was fixed

supervised_learning/tensorflow/4-calculate_loss.py
@@ -14,7 +14,5 @@ def calculate_accuracy(y, y_pred):
     """
 
     # Calculate softmax cross-entropy loss:
-    cross_e = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_pred)
-    loss = tf.reduce_mean(cross_e)
-
-    return loss
+    cross_e = tf.losses.softmax_cross_entropy(y, logits=y_pred)
+    return cross_e