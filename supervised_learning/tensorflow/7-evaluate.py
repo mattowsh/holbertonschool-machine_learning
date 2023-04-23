306ce72155c00037ccff5ea9be3d3c09b39dbb2a
mattowsh <mattobelen@gmail.com>
2023-04-23 18:52:08 -0300

    new try, for checker

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -22,17 +22,28 @@ def evaluate(X, Y, save_path):
         saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
 
-        # Get the data restored:
-        X = tf.get_collection("X")[0]
-        Y = tf.get_collection("Y")[0]
-        Y_pred = tf.get_collection("Y_pred")[0]
+        # # Get the data restored:
+        # X = tf.get_collection("X")[0]
+        # Y = tf.get_collection("Y")[0]
+        # Y_pred = tf.get_collection("Y_pred")[0]
+        # accuracy = tf.get_collection("accuracy")[0]
+        # loss = tf.get_collection("loss")[0]
+
+        # # Run the evaluation:
+        # Y_eval = sess.run(Y_pred, feed_dict={X: X, Y: Y})
+        # accuracy_eval = sess.run(accuracy, feed_dict={X: X, Y: Y})
+        # loss_eval = sess.run(loss, feed_dict={X: X, Y: Y})
+
+        # # Final return:
+        # return Y_eval, accuracy_eval, loss_eval
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
 
-        # Run the evaluation:
-        Y_eval = sess.run(Y_pred, feed_dict={X: X, Y: Y})
-        accuracy_eval = sess.run(accuracy, feed_dict={X: X, Y: Y})
-        loss_eval = sess.run(loss, feed_dict={X: X, Y: Y})
-
-        # Final return:
-        return Y_eval, accuracy_eval, loss_eval
+        y_eval = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy_eval = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_eval = sess.run(loss, feed_dict={x: X, y: Y})
+        return y_eval, accuracy_eval, loss_eval
