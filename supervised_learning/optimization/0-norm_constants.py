60effb3e071e571183225f061d1b1ed64635bd93
mattowsh <mattobelen@gmail.com>
2023-04-26 21:33:00 -0300

    task 0 v0

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 0. Normalization Constants
+"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Calculates the normalization constants of a matrix
+
+        X: numpy.ndarray of shape (m, nx) to normalize
+            m: number of data points
+            nx: number of features
+    """
+ 
+    # Calculates the mean and std deviation:
+    return np.mean(X, axis=0), np.std(X, axis=0)
