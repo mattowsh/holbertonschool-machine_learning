dc12ade8538f5bff1534f952de30db25999efbe4
mattowsh <mattobelen@gmail.com>
2023-04-27 12:15:42 -0300

    task 5 v0

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Task 5. Momentum
+"""
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using the gradient descent with momentum optimization
+    algorithm
+
+        alpha: learning rate
+        beta: momentum weight
+        var: numpy.ndarray containing the variable to be updated
+        grad: numpy.ndarray containing the gradient of var
+        v: previous first moment of var
+    """
+
+    # Update the momentum vector:
+    momentum = beta1 * v + (1 - beta1) * grad
+
+    # Update the variable value:
+    updated_value = var - (alpha * momentum)
+
+    # Final return:
+    return updated_value, momentum
