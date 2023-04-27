7353c39593646b509240346c281e9e163c7ca261
mattowsh <mattobelen@gmail.com>
2023-04-26 21:48:02 -0300

    task1 v0

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 2. Shuffle Data
+"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+	"""
+    Shuffles the data points in two matrices the same way
+
+        X: numpy.ndarray of shape (m, nx) to shuffle
+            m: number of data points
+            nx: number of features in X
+        X: numpy.ndarray of shape (m, ny) to shuffle
+            m: same number of data points as in X
+            ny: number of features in Y
+    """
+    
+	X_perm = np.random.permutation(len(X))
+    
