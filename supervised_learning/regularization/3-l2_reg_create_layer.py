a4a87adc9b5749b222ac6b0964a4e4b62d3ea83f
mattowsh <mattobelen@gmail.com>
2023-05-06 22:25:30 -0300

    task 3 v0

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 """
-Task 1. Gradient Descent with L2 Regularization
+Task 3. Create a Layer with L2 Regularization
 """
 import tensorflow as tf
 
 
-def l2_reg_cost(cost):
+def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Calculates the cost of a neural network with L2 regularization
-    using Numpy and TensorFlow
+    Creates a tensorflow layer that includes L2 regularization
 
-    cost: a tensor containing the cost of the network without L2 regularization
+    prev: a tensor containing the output of the previous layer
+    n: the number of nodes the new layer should contain
+    activation: the activation function that should be used on the layer
+    lambtha: L2 regularization parameter
 
-    Returns: a tensor containing the cost of the network accounting for L2
-    regularization
+    Returns: the output of the new layer
     """
 
-    return cost + tf.losses.get_regularization_losses()
+    regularizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
+    new_layer = tf.layers.dense(prev, n, activation=activation,
+                                kernel_regularizer=regularizer)
+
+    return new_layer
