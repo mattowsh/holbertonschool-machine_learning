944f8c69c6bc69d880777557fc25e09f2e905615
mattowsh <mattobelen@gmail.com>
2023-05-11 13:06:47 -0300

    task 1 v0

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -20,5 +20,26 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     Returns: the keras model
     """
-    
-	
+
+    # Set the deep learning regularization technique:
+    regularizer = K.regularizers.l2(lambtha)
+
+    # Define the input layer:
+    input_layer = K.Input(shape=(nx,))
+    other_layers = input_layer
+
+    # Define the model architecture:
+    for i in range(len(layers)):
+        if i != 0:
+            # Define hidden and output layers. Input layer (layers[0]) does not
+            # contain Dropout:
+            other_layers = K.layers.Dropout(1 - keep_prob)(other_layers)
+
+        other_layers = K.layers.Dense(layers[i],
+                                      activation=activations[i],
+                                      kernel_regularizer=regularizer
+                                      )(other_layers)
+
+    # Create the model:
+    model = K.Model(inputs=input_layer, outputs=other_layers)
+    return model
