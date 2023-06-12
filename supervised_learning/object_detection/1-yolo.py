3b5f6def5810374dc68769f69854b4511368ca4d
mattowsh <mattobelen@gmail.com>
2023-06-11 23:09:33 -0300

    task 1 v0

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -3,6 +3,7 @@
 Task 0. Process outputs
 """
 import tensorflow.keras as K
+import numpy as np
 
 
 class Yolo:
@@ -38,14 +39,21 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-        
+
+    @staticmethod
+    def sigmoid(x):
+        """
+        Performs Sigmoid activation function
+        """
+        return 1 / (1 + np.exp(-x))
+
     def process_outputs(self, outputs, image_size):
         """
         Method to process Yolo algorithm outputs
-        
+
         - outputs: list of numpy.ndarrays containing the predictions from the
         Darknet model for a single image
-        
+
         Note: Each output will have the shape
         (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
             - grid_height & grid_width => the height and width of the grid used
@@ -74,4 +82,72 @@ class Yolo:
             probabilities for each output, respectively
         """
 
-        
+        boxes, box_confidences, box_class_probs = [], [], []
+        img_h, img_w = image_size
+        full_anchor_w = self.anchors[:, :, 0]
+        full_anchor_h = self.anchors[:, :, 1]
+
+        # Process each prediction, stored in outputs list
+        for i, predict in enumerate(outputs):
+            # 1. Get dimensions of the current prediction:
+            grid_h, grid_w, anchor_boxes, _ = predict
+
+            # 2. Get each prediction dimension:
+            t_x = predict[:, :, :, 0]
+            t_y = predict[:, :, :, 1]
+            t_w = predict[:, :, :, 2]
+            t_h = predict[:, :, :, 3]
+
+            # 3. Reshape anchor box measures to match grid dimensions:
+            current_anchor_h = np.tile(full_anchor_h[i], grid_h)
+            current_anchor_h = current_anchor_h.reshape(grid_h, 1,
+                                                        len(full_anchor_h[i]))
+
+            current_anchor_w = np.tile(full_anchor_w[i], grid_w)
+            current_anchor_w = current_anchor_w.reshape(grid_w, 1,
+                                                        len(full_anchor_w[i]))
+
+            # 4. Create grid coordinates for positioning boundary boxes:
+            gc_x = np.tile(np.arange(grid_w), grid_h).reshape(grid_w,
+                                                              grid_w, 1)
+            gc_y = np.tile(np.arange(grid_w),
+                           grid_h).reshape(grid_h, grid_h).T.reshape(grid_h,
+                                                                     grid_h, 1)
+
+            # 5. Boundary boxes: empty array to store processed boundary boxes:
+            box = np.zeros(predict[:, :, :, :4].shape)
+
+            # 6. Boundary boxes: get the absolute coordinates:
+            b_x = (self.sigmoid(-t_x) + gc_x) / grid_w
+            b_y = (self.sigmoid(-t_y) + gc_y) / grid_h
+
+            # 7. Boundary boxes: get the dimensions:
+            b_h = current_anchor_h * np.exp(t_h)
+            b_w = current_anchor_w * np.exp(t_w)
+
+            # 8. Store boxes coordinates and dimensions:
+            # top-left coordinate: represents the upper-left corner of the
+            # bounding box:
+            x1 = (b_x - (b_w / 2)) * img_w
+            y1 = (b_y - (b_h / 2)) * img_h
+
+            # bottom-right coordinate: represents the lower-right corner of the
+            # bounding box:
+            x2 = (b_x + (b_w / 2)) * img_w
+            y2 = (b_y + (b_h / 2)) * img_h
+
+            box[:, :, :, 0] = x1
+            box[:, :, :, 1] = y1
+            box[:, :, :, 2] = x2
+            box[:, :, :, 0] = y2
+            boxes.append(box)
+
+            # 9. Get the box confidences for each output:
+            ax = self.sigmoid(predict[:, :, :, 4])
+            box_confidences.append(ax.reshape(grid_h, grid_w, anchor_boxes, 1))
+
+            # 10. Calculate the box class probabilities:
+            ax = predict[:, :, :, 5:]
+            box_class_probs.append(self.sigmoid(ax))
+
+        return boxes, box_confidences, box_confidences
